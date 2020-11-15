71d54b9f92575f7c64884c09dc5c515dd4967449
Kurt Newcomb <blackabyss25@gmail.com>
2020-11-14 19:38:49 -0800

    update #2 for registration

--- CST247CLC/Services/Data/SecurityDAO.cs
+++ CST247CLC/Services/Data/SecurityDAO.cs
@@ -33,14 +33,12 @@ namespace CST247CLC.Services.Data
 
         public bool Register(User user)
         {
-            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Minesweeper;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            string query = $"INSERT INTO dbo.Users rtrim(USERNAME, PASSWORD, FIRSTNAME, LASTNAME, SEX, AGE, STATE, EMAIL) VALUES (@Username, @Password, @FirstName, @Lastname, @Sex, @Age, @State, @Email)";
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string query = $"INSERT INTO Users (USERNAME, PASSWORD, FIRSTNAME, LASTNAME, SEX, AGE, STATE, EMAIL) VALUES (@Username, @Password, @FirstName, @Lastname, @Sex, @Age, @State, @Email)";
             bool results = false;       //default assumption of result
             //Check to see if the user data made it to the server in a valid configuration, meaning that all of the fields have something in them, and the email field contains an @ sign, could be upgraded later.
-            if (validateUser(user))
+            if (ValidateUser(user))
             {
-
-                //results = true;//Confirmed this works up to this point...  Starting here it doesn't
                 using (SqlConnection con = new SqlConnection(connectionString)) //'using' ensures connections are closed after use.
                 {
                     SqlCommand comm = new SqlCommand(query, con);
@@ -55,11 +53,12 @@ namespace CST247CLC.Services.Data
                     try
                     {
                         comm.Connection.Open();
-                        
-                        SqlDataReader reader = comm.ExecuteReader();
-                        if (reader.HasRows) { results = true; }
+                        comm.ExecuteNonQuery();
+                        results = true;
                     }
-                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                    catch (Exception ex) { 
+                        Console.WriteLine(ex.Message); 
+                    }
                 }
 
             }
@@ -68,43 +67,21 @@ namespace CST247CLC.Services.Data
 
         //Runs the server-side validation of the user-input data
         //Currently, the validation is only checking for non-empty data fields
-        public bool validateUser(User user)
+        public bool ValidateUser(User user)
         {
             bool validUser = false;
-            if (user.FirstName.Length != 0)
+            if (user.FirstName.Length != 0 && 
+                user.LastName.Length != 0 && 
+                user.Sex.Length != 0 && 
+                user.Age.ToString().Length != 0 && 
+                user.State.ToString().Length != 0 &&
+                (user.Email.ToString().Length != 0 || !user.Email.Contains("@")) &&
+                user.Username.Length != 0 &&
+                user.Password.Length != 0)
             {
                 validUser = true;
             }
-            else if (user.LastName.Length != 0)
-            {
-                validUser = true;
-            }
-            else if (user.Sex.Length != 0)
-            {
-                validUser = true;
-            }
-            //Don't care what the number is, just want to make sure there is something there.
-            else if (user.Age.ToString().Length != 0)
-            {
-                validUser = true;
-            }
-            else if (user.State.ToString().Length != 0)
-            {
-                validUser = true;
-            }
-            //If it's empty or doesn't have an @, fail the validation check
-            else if (user.Email.ToString().Length != 0 || !user.Email.Contains("@"))
-            {
-                validUser = true;
-            }
-            else if (user.Username.Length != 0)
-            {
-                validUser = true;
-            }
-            else if (user.Password.Length != 0)
-            {
-                validUser = true;
-            }
+            Console.Out.WriteLine("validateUser: ", validUser);
             return validUser;
         }
 
