e9b0c699c7c9447080cc4aa3b501df3f35e0525a
Isaac Tucker <isaac.takka@gmail.com>
2020-11-17 20:14:24 -0700

    not done

--- Application/Controllers/MinesweeperController.cs
+++ Application/Controllers/MinesweeperController.cs
@@ -9,11 +9,54 @@ namespace CST247CLC.Controllers
 {
     public class MinesweeperController : Controller
     {
-        static public Board board = new Board(10);
+        static public Board myBoard = new Board(10);
         // GET: Minesweeper
         public ActionResult Index()
         {
-            return View("Minesweeper", board);
+            return View("Minesweeper", myBoard);
+        }
+
+        public ActionResult OnButtonClick(string MineID)
+        {
+            string[] strArr = MineID.Split('|');
+            int r = int.Parse(strArr[0]);
+            int c = int.Parse(strArr[1]);
+
+            Cell currentCell = myBoard.grid[r, c];
+
+            //run game logic
+            if (!currentCell.isFlagged)
+                gameLogic(currentCell);
+
+            /*
+            if (me.Button == MouseButtons.Left)
+            {
+                if (!currentCell.isFlagged)
+                    gameLogic(currentCell);
+            }
+            else if (me.Button == MouseButtons.Right)
+            {
+                if (!currentCell.isVisited)
+                {
+                    currentCell.isFlagged = !currentCell.isFlagged;
+                    updateButtonLabels();
+                }
+            } */
+
+            return View("Minesweeper", myBoard);
+        }
+
+        private void gameLogic(Cell currentCell)    //send logic to gameBoard for process and update the views accordingly.
+        {
+            if (!currentCell.isVisited)
+            {
+                //check if cell is a bomb
+                myBoard.checkForBomb(currentCell);
+                //have all non bomb cells been visisted?
+                //else if (myBoard.checkForVictory()) { GameOver("win"); return; }
+                //update the buttons
+                //else { //updateButtonLabels(); }
+            }
         }
     }
 }
