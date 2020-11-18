a747c8b224e47e259591a6326ff67ba75eb73b98
Isaac Tucker <isaac.takka@gmail.com>
2020-11-17 19:21:38 -0700

    Aaaaaa

---
+++ Application/Controllers/MinesweeperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CST247CLC.Controllers
+{
+    public class MinesweeperController : Controller
+    {
+        // GET: Minesweeper
+        public ActionResult Index()
+        {
+            return View("Minesweeper");
+        }
+    }
+}
